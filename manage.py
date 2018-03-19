206c7fd3532d5d32fe1249fd0174c75113048713
heqiong <heqiong@onething.net>
2018-03-19 14:11:18 +0800

    是

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ from flask_script import Manager
 from myapp.record.views import record as record
 from myapp.models import app
 
+app.register_blueprint(record)
 manager = Manager(app)
 
 if __name__ == '__main__':
